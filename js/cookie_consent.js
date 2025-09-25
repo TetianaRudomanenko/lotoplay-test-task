e614e0971eaa0a75776ea24b8d79121f0d615547
Tetiana Rudomanenko <rudomanenko.tatyana@gmail.com>
2025-09-25 02:07:13 +0200

    Add TarteAuCitron.js for GDPR cookie consent

---
+++ js/cookie_consent.js
@@ -0,0 +1,53 @@
+tarteaucitron.init({
+  "privacyUrl": "",
+
+  "bodyPosition": "top",
+  "hashtag": "#tarteaucitron",
+  "cookieName": "tarteaucitron",
+
+  "orientation": "middle",
+  "groupServices": true,
+  "showDetailsOnClick": true,
+  "serviceDefaultState": "wait",
+
+  "showAlertSmall": false,
+  "cookieslist": false,
+
+  "closePopup": true,
+  "showIcon": true,
+  "iconSrc": "/images/cookie.svg",
+  "iconPosition": "BottomRight",
+
+  "adblocker": false,
+
+  "DenyAllCta": true,
+  "AcceptAllCta": true,
+  "highPrivacy": true,
+  "alwaysNeedConsent": false,
+
+  "handleBrowserDNTRequest": false,
+
+  "removeCredit": false,
+  "moreInfoLink": true,
+
+  "useExternalCss": false,
+  "useExternalJs": false,
+
+  //"cookieDomain": ".my-multisite-domaine.fr",
+
+  "readmoreLink": "",
+
+  "mandatory": true,
+  "mandatoryCta": false,
+
+  //"customCloserId": "",
+
+  "googleConsentMode": true,
+  "bingConsentMode": true,
+  "softConsentMode": false,
+
+  "dataLayer": false,
+  "serverSide": false,
+
+  "partnersList": true
+});
